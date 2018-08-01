7a351baca7dbe86e80e340960c5ecae1dfce0f41
Tristan Hall <tristanalex1986@gmail.com>
2018-08-01 11:27:57 +0100

    now able to create reviews. Working on a reviews/show page

--- config/routes.js
+++ config/routes.js
@@ -7,20 +7,15 @@ const reviewController = require('../controllers/reviewController');
 
 router.get('/', (req, res) => res.render('pages/_home'));
 router.get('/_about', (req, res) => res.render('pages/_about'));
-
 router.get('/sites/:id/edit', siteController.edit);
-
-router.route('/sites/index')
-  .get(siteController.index);
 
 router.route('/registrations/new')
   .get(registrationController.new);
 
-router.route('/reviews/new')
-  .post(reviewController.create);
-
 router.route('/registrations')
   .post(registrationController.create);
+
+// router.route('/reviews/new')
 
 router.route('/sessions/new')
   .get(sessionController.new);
@@ -35,13 +30,19 @@ router.route('/sites/new')
   .get(siteController.new);
 
 router.route('/sites')
+  .get(siteController.index)
   .post(siteController.create);
+
+router.route('/sites/:siteId/review')
+  .post(reviewController.create);
 
 router.route('/sites/:id')
   .get(siteController.show)
   .put(siteController.update)
   .delete(siteController.delete);
 
+router.route('/sites/:siteId/reviews')
+  .post(reviewController.create);
 
 
 
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,13 +3,16 @@ const Site = require('../models/site');
 function reviewsCreate(req, res) {
   console.log('we are in reviews create controller');
   Site
-    .findById(req.body.id)
+    .findById(req.params.siteId)
     .then(site => {
+      console.log('AT LEAST YOU GOT THIS FAR...');
       site.reviews.push(req.body);
       return site.save();
     })
     .then(site => res.redirect(`/sites/${site.id}`))
     .catch(err => console.log(err));
+
+
 }
 
 
